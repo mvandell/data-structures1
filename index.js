e45f544aad43a03e63875021aa488a365317134a
Marisa <marisavandellos@gmail.com>
2023-12-19 15:27:11 -0800

    added start of part 2

--- index.js
+++ index.js
@@ -45,14 +45,3 @@ function validBrackets(string) {
 }
 
 console.log(s, validBrackets(s))
-
-//<------------------------------------------PART 2---------------------------------------------->
-const tickets = [2,3,2];
-const k = 2;
-let time = 0;
-
-for (let i = 0; i < tickets.length; i++) {
-    if (tickets[i] === 1) {
-        
-    }
-}
