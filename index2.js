5dc65d62a54c05547071271b13d62cd75fc43098
Marisa <marisavandellos@gmail.com>
2023-12-19 15:59:19 -0800

    part 2 done

--- index2.js
+++ index2.js
@@ -3,8 +3,24 @@ const tickets = [2,3,2];
 const k = 2;
 let time = 0;
 
-for (let i = 0; i < tickets.length; i++) {
-    if (tickets[i] === 1) {
-        
+tickets[k] += 0.1;
+
+let i = 0;
+while (i<tickets.length) {
+
+    if (tickets[i] === 1.1) {
+        tickets.shift();
+        time++;
+        break;   
+    } else if (tickets[i] === 1) {
+        tickets.shift();
+        time++;
+    } else{
+        tickets[i]--;
+        let hold = tickets.shift();
+        tickets.push(hold);
+        time++;
     }
 }
+
+console.log(time);
