95b599acfd62ab7635a8d46fcf2ddcdaa749d5d9
Marisa <marisavandellos@gmail.com>
2023-12-20 09:25:10 -0800

    still working on part 4

--- index4.js
+++ index4.js
@@ -6,10 +6,10 @@ let i = deck.length - 1;
 while (deck.length > 0) {
     if (deck[i-1]) {
         stack.push(deck.pop());
-        stack.push(deck.pop());
+        i--;
+        stack.unshift(deck.pop());
     }
     else {stack.push(deck.pop())}
-    i -= 2;
 }
 
 console.log(stack)
